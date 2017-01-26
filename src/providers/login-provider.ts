cca81b133e9523a6a1f411ad767cc391503a8b37
luan-prates <pratesr21@gmail.com>
2017-01-26 10:52:08 -0300

    usando Ngzone e EventEmitter

--- src/providers/login-provider.ts
+++ src/providers/login-provider.ts
@@ -1,20 +1,45 @@
-import { Injectable } from '@angular/core';
+import {Injectable, EventEmitter, NgZone, state} from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import {Credencial} from "../model/credencial";
 import firebase from "firebase";
 
-/*
-  Generated class for the LoginProvider provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class LoginProvider {
 
-  constructor(public http: Http) {
-    console.log('Hello LoginProvider Provider');
+  currentUesr:any;
+  autenticado:boolean;
+
+  loginSucessoEventEmitter:EventEmitter<any>;
+  loginFalhaEventEmitter:EventEmitter<any>;
+  logoutEventEmitter:EventEmitter<any>;
+
+  constructor(public http: Http, public ngZone: NgZone) {
+    this.loginSucessoEventEmitter = new EventEmitter();
+    this.loginFalhaEventEmitter = new EventEmitter();
+    this.logoutEventEmitter = new EventEmitter();
+
+    firebase.auth().onAuthStateChanged(usuario => {
+      this.callbackStateChange(usuario);
+    })
+  }
+
+  private callbackStateChange(usuario){
+    this.ngZone.run( () => {
+      if(usuario == null){
+        this.currentUesr = null;
+        this.autenticado = false;
+      }else{
+        this.currentUesr = usuario;
+        this.autenticado = true;
+      }
+    })
+  }
+
+  loginComCredencial(credencial:Credencial){
+    firebase.auth().signInWithEmailAndPassword(credencial.email, credencial.senha)
+      .then(resultado => this.callbackSucessoLogin(resultado))
+      .catch(error => this.callbackFalhaLogin(error))
   }
 
   redistrarUsuario(credencial:Credencial){
@@ -23,4 +48,16 @@ export class LoginProvider {
       .catch(error => console.log(error));
   }
 
+  private callbackSucessoLogin(response){
+    this.loginSucessoEventEmitter.emit(response.user);
+  }
+
+  private callbackFalhaLogin(error){
+    this.loginFalhaEventEmitter.emit({
+      code: error.code,
+      message: error.message,
+      email: error.email,
+      credencial: error.credencial
+    });
+  }
 }
